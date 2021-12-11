5a508471370ee28643d128f4440f75ac1d5ced0f
Remix23 <61829560+Remix23@users.noreply.github.com>
2021-12-11 01:04:57 +0100

    Day 9 - second part

--- src/main/kotlin/day9/day9.kt
+++ src/main/kotlin/day9/day9.kt
@@ -4,15 +4,17 @@ import java.io.File
 
 fun main ()
 {
-    val board = loadMap("C:\\Users\\keruj\\IdeaProjects\\KotlinAdventOfCode\\src\\main\\inputs\\day9.txt")
+    val board = loadMap("C:\\Users\\keruj\\IdeaProjects\\KotlinAdventOfCode\\src\\main\\inputs\\in9.txt")
 
     val lowPoints = getLowPoints(board)
 
-    var sumOfRiskLevels = 0
+    val basinSizes = mutableListOf<Int>()
     lowPoints.forEach{
-        sumOfRiskLevels += it + 1
+        basinSizes.add(getBasinFromLowPoint(it.first, it.second, board))
+        println(basinSizes.last())
     }
-    println(sumOfRiskLevels)
+    basinSizes.sort()
+    println(basinSizes[lowPoints.size - 1] * basinSizes[lowPoints.size - 2] * basinSizes[lowPoints.size - 3])
 }
 
 fun loadMap (path : String) : Array<Array<Int>>
@@ -28,16 +30,45 @@ fun loadMap (path : String) : Array<Array<Int>>
     return board.toTypedArray()
 }
 
-fun getLowPoints (board: Array<Array<Int>>) : MutableList<Int>
+fun getBasinFromLowPoint (x : Int, y : Int, board: Array<Array<Int>>) : Int
 {
-    val lowPoints = mutableListOf<Int>()
+    val basin = mutableSetOf<Pair<Int, Int>>()
+    val toCheck = mutableSetOf<Pair<Int, Int>>()
+    toCheck.add(Pair(x, y))
+
+    while (toCheck.size > 0)
+    {
+        val current = toCheck.first()
+
+        basin.add(current)
+        toCheck.remove(current)
+
+        val neighbours = getNeighbours(current.first, current.second, board)
+
+        for (n in neighbours)
+        {
+            if (validateCords(n.first, n.second, board[0].size, board.size))
+            {
+                if ( board[n.second][n.first] > board[current.second][current.first] && board[n.second][n.first] < 9)
+                {
+                    toCheck.add(n)
+                }
+            }
+        }
+    }
+    return basin.size
+}
+
+fun getLowPoints (board: Array<Array<Int>>) : MutableList<Pair<Int, Int>>
+{
+    val lowPoints = mutableListOf<Pair<Int, Int>>()
     for (y in board.indices)
     {
         for (x in board[y].indices)
         {
             if (isLowPoint(x, y, board))
             {
-                lowPoints.add(board[y][x])
+                lowPoints.add(Pair(x, y))
             }
         }
     }
@@ -47,19 +78,27 @@ fun getLowPoints (board: Array<Array<Int>>) : MutableList<Int>
 fun isLowPoint (x : Int, y : Int, board : Array<Array<Int>>) : Boolean
 {
     val current = board[y][x]
+    val neighbours = getNeighbours(x, y, board)
+    neighbours.forEach {
+        if (current >= board[it.second][it.first]) return false
+    }
+    return true
+}
+
+fun getNeighbours (x : Int, y : Int, board: Array<Array<Int>>) : MutableList<Pair<Int, Int>>
+{
+    val neighbors = mutableListOf<Pair<Int, Int>>()
     for (i in -1..1)
     {
         for (j in -1..1)
         {
             if (validateCords(x + j, y + i, board[0].size, board.size) && (i == 0 || j == 0) && !(i == 0 && j == 0))
             {
-                if (board[y + i][x + j] <= current) {
-                    return false
-                }
+                neighbors.add(Pair(x + j, y + i))
             }
         }
     }
-    return true
+    return neighbors
 }
 
 fun validateCords (x : Int, y : Int, maxX : Int, maxY : Int) : Boolean
